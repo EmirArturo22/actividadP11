bddb20f71c1e515f337a436bf313e98d149a0a29
EmirArturo22 <145036472+EmirArturo22@users.noreply.github.com>
2023-09-21 09:37:14 -0600

    Create cod_olimpiadas1.py

---
+++ cod_olimpiadas1.py
@@ -0,0 +1,27 @@
+#ronda 1
+def multi(a,b):
+  x= a*b
+  return x
+  print("dame un numero")
+  a= int(input())
+  print("dame otro numero")
+  b= int(input())
+  print("la division da", division(a,b),"y la multi da", multi(a,b))
+#ronda 2
+def convercion(kilometros):
+  print("cantidad de kilometros que quieres convertir")
+  kilometros=input()
+  print("te dare tus kilometros en metros")
+  b=int(kilometros)/100
+  print("el resultado",(metros))
+conversion()
+
+#ronda 3
+def area_triangulo(base, altura):
+ x= (base*altura)/2
+  return x
+print("dame la base del triangulo")
+base= int(input())
+print("dame la altura del triangulo")
+altura= int(input())
+print("tu area del triangulo es"area_triangulo(base,altura))
